4eea722787466ae1e44cc50cdfbd46592c8aa9fe
Γιάννης Μικές <mikesgiannis@gmail.com>
2022-05-17 17:53:59 +0200

    Changed listening port to 3001

--- YelpBackend/server.js
+++ YelpBackend/server.js
@@ -40,6 +40,6 @@ app.get("/business/:id", (req, res) => {
 	//res.json(result);
 })
 
-app.listen(3000, () => {
-	console.log("listening to port 3000...");
+app.listen(3001, () => {
+	console.log("listening to port 3001...");
 })

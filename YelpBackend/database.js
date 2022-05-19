67253778273aea08dec82d8a30483f7177f26bee
Γιάννης Μικές <mikesgiannis@gmail.com>
2022-05-19 10:26:08 +0200

    Changed database location from local to cloud

--- YelpBackend/database.js
+++ YelpBackend/database.js
@@ -1,10 +1,15 @@
 const knex = require("knex")({
 	client: "mysql2",
 	connection: {
-		host: "localhost",
-		database: "yelp",
-		user: "root",
-		password: "13334921",
+		//host: "localhost",
+		//database: "yelp",
+		//user: "root",
+		//password: "13334921",
+		//port: "3306"
+		host: "sql11.freesqldatabase.com",
+		database: "sql11493069",
+		user: "sql11493069",
+		password: "48WWNPbM3p",
 		port: "3306"
 	}
 });

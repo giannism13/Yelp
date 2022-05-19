bcda9c06f07360672aee6de68c33bae32105c903
Γιάννης Μικές <mikesgiannis@gmail.com>
2022-05-19 10:26:40 +0200

    Cleanup

--- YelpBackend/database.js
+++ YelpBackend/database.js
@@ -1,11 +1,6 @@
 const knex = require("knex")({
 	client: "mysql2",
 	connection: {
-		//host: "localhost",
-		//database: "yelp",
-		//user: "root",
-		//password: "13334921",
-		//port: "3306"
 		host: "sql11.freesqldatabase.com",
 		database: "sql11493069",
 		user: "sql11493069",

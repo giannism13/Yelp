89917d53189106fd47ffa1cdf4a6e542883a9624
Γιάννης Μικές <mikesgiannis@gmail.com>
2022-05-24 22:53:42 +0200

    Styling overhaul

--- YelpFrontend/src/components/ListingItem.js
+++ YelpFrontend/src/components/ListingItem.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useCallback } from "react";
 import placeholder from "../../src/placeholder.jpg"
+import { useNavigate } from 'react-router-dom';
+
 
 function ListingItem(props) {
+	const { listingItem } = props;
+	const navigate = useNavigate();
+	const onClick = useCallback(() => {
+		navigate(`/business?id=${listingItem.business_id}`);
+	}, [navigate, listingItem])
+
 	return (
-		<div className="flex items-center justify-between w-50%">
-			<div className="flex flex-col lg:flex-row w-full items-start lg:items-center rounded bg-white shadow">
-				<div className="w-full lg:w-1/3 h-24 lg:h-64 border-t lg:border-t-0 lg:border-r lg:border-l lg:rounded-r bg-gray-100">
-					<img src={placeholder} width="50%" height="50%" />
-				</div>
-				<div className="w-full lg:w-2/3 h-64">
-					<h1>{props.listingItem.name}</h1>
-					<h2>{props.listingItem.stars} / {props.listingItem.review_count}</h2>
+		<div className="flex justify-center">
+			<div className="flex justify-center cursor-pointer m-5 rounded-lg bg-white shadow-xl overflow-hidden w-full max-w-6xl" onClick={onClick}>
+				<div className="flex flex-col lg:flex-row w-full items-start lg:items-center rounded">
+					<div style={{ backgroundImage: `url(${placeholder})` }} className="w-64 h-24 lg:h-48 bg-gray-100 bg-center bg-cover bg-no-repeat	">
+					</div>
+					<div className="w-full lg:w-2/3 h-48 p-5">
+						<h1><span className="text-2xl"><b>{listingItem.name}</b></span></h1>
+						<h2>{listingItem.stars} / {listingItem.review_count}</h2>
+					</div>
 				</div>
 			</div>
 		</div>

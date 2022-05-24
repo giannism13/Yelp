3ec34bcb3c6d0d9f5ee33e61c63ae35e7b6f9eaf
Γιάννης Μικές <mikesgiannis@gmail.com>
2022-05-24 22:52:54 +0200

    implemented react router navigation

--- YelpFrontend/src/App.js
+++ YelpFrontend/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 import Index from './pages/Index.js';
+import Business from './pages/Business';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
 	return (
-		<Index />
+		<>
+			<Routes>
+				<Route path="/" element={<Index />} />
+				<Route path="business" element={<Business />} />
+			</Routes>
+		</>
 	);
 }
 

b7021a1b906d5e0205b8a8c16a210a24e13d9ddc
Γιάννης Μικές <mikesgiannis@gmail.com>
2022-06-01 02:54:33 +0200

    Listing items now display an appropriate photo as a cover

--- YelpFrontend/src/components/ListingItem.js
+++ YelpFrontend/src/components/ListingItem.js
@@ -1,5 +1,4 @@
-import React, { useCallback } from "react";
-import placeholder from "../../src/placeholder.jpg"
+import React, { useCallback, useEffect, useState } from "react";
 import star from "../../src/star.png"
 import { useNavigate } from 'react-router-dom';
 
@@ -7,15 +6,28 @@ import { useNavigate } from 'react-router-dom';
 function ListingItem(props) {
 	const { listingItem } = props;
 	const navigate = useNavigate();
+	const [cover, setCover] = useState([]);
+
 	const onClick = useCallback(() => {
 		navigate(`/business?id=${listingItem.business_id}`);
 	}, [navigate, listingItem])
+
+	useEffect(() => {
+		fetch(`http://localhost:3001/photos1/${listingItem.business_id}`)
+			.then(response => response.json())
+			.then(
+				data => {
+					setCover(data);
+					console.log(data);
+				}
+			);
+	}, [listingItem.business_id]);
 
 	return (
 		<div className="flex justify-center">
 			<div className="flex justify-center cursor-pointer m-5 rounded-lg bg-white shadow-xl overflow-hidden w-full max-w-6xl" onClick={onClick}>
 				<div className="flex flex-col lg:flex-row w-full items-start lg:items-center rounded">
-					<div style={{ backgroundImage: `url(${placeholder})` }} className="w-64 h-24 lg:h-48 bg-gray-100 bg-center bg-cover bg-no-repeat	">
+					<div style={{ backgroundImage: `url(${process.env.PUBLIC_URL + "images/" + cover.photo_id + ".jpg"})` }} className="w-64 h-24 lg:h-48 bg-gray-100 bg-center bg-cover bg-no-repeat	">
 					</div>
 					<div className="w-full lg:w-2/3 h-48 p-5">
 						<h1><span className="text-2xl"><b>{listingItem.name}</b></span></h1>

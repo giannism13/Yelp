8afe4ef9bb3a76d67fb0e97a554a62a3e348b4de
Zehra Yildiz <zehrayildiz@Zehras-MacBook-Air.local>
2022-06-20 15:45:49 +0200

    initial commit

--- YelpBackend/server.js
+++ YelpBackend/server.js
@@ -9,61 +9,68 @@ app.use(cors());
 
 //endpoint for a future list of categories page
 app.get("/categories", (req, res) => {
-	const object = knex("businesses").select("categories").then((response) => {
-		res.send(response);
-	});
-	console.log("GET categories request");
+    const object = knex("businesses").select("categories").then((response) => {
+        res.send(response);
+    });
+    console.log("GET categories request");
+})
+
+//endpoint for a future list of businesses for category
+app.get("/businessByCategorie", (req, res) => {
+    const object = knex("businesses").select("address", "attributes.Open24Hours").where("categories", req.params.q).then((response) => {
+        res.send(response);
+    });
+    console.log("GET categories request");
 })
 
 //endpoint for a future search function
 //seach query must be passed as query parameter eg. /search/?q=rental
 app.get("/search/", (req, res) => {
-	var searchQ = req.query.q;
+    var searchQ = req.query.q;
 
-	const object = knex("businesses").select("name", "business_id", "stars", "longitude", "latitude", "review_count").
-		whereILike("name", `%${searchQ}%`).then((response) => {
-			//console.log(response);
-			var numberOfResults = Object.keys(response).length;
-			//res.write('' + numberOfResults)	// we have to convert the number to a string in order to be sented
-			res.send(['' + numberOfResults, response]);
-		})
-	console.log("GET search request. Q= " + searchQ);
+    const object = knex("businesses").select("name", "business_id", "stars", "longitude", "latitude", "review_count").
+    whereILike("name", `%${searchQ}%`).then((response) => {
+        //console.log(response);
+        var numberOfResults = Object.keys(response).length;
+        //res.write('' + numberOfResults)	// we have to convert the number to a string in order to be send
+        res.send(['' + numberOfResults, response]);
+    })
+    console.log("GET search request. Q= " + searchQ);
 })
 
 //endpoint for when you click on a business
 //request should be at /business/some_business_id
 app.get("/business/:id", (req, res) => {
-	var businessID = req.params.id;
-	let result = knex("businesses").where("business_id", businessID).first()
-	result.then((response) => {
-		console.log(response);
-		res.send(response);
-	})
-	console.log("GET business request. id= " + businessID);
+    var businessID = req.params.id;
+    let result = knex("businesses").where("business_id", businessID).first()
+    result.then((response) => {
+        console.log(response);
+        res.send(response);
+    })
+    console.log("GET business request. id= " + businessID);
 })
 
 //image filename request endpoint
 app.get("/imgFilenames/:business_id", (req, res) => {
-	var businessID = req.params.business_id;
-	var number = req.query.q;
+    var businessID = req.params.business_id;
+    var number = req.query.q;
 
-	if (number == 1) {
-		const object = knex("images").select("photo_id", "caption").
-			whereILike("business_id", businessID).first().then((response) => {
-				res.send(response);
-			})
-	}
-	else {
-		const object = knex("images").select("photo_id", "caption").
-			whereILike("business_id", businessID).then((response) => {
-				res.send(response);
-			})
-	}
-	//console.log("GET photo request. Q= " + searchQ);
+    if (number == 1) {
+        const object = knex("images").select("photo_id", "caption").
+        whereILike("business_id", businessID).first().then((response) => {
+            res.send(response);
+        })
+    } else {
+        const object = knex("images").select("photo_id", "caption").
+        whereILike("business_id", businessID).then((response) => {
+            res.send(response);
+        })
+    }
+    //console.log("GET photo request. Q= " + searchQ);
 })
 
 app.use('/images', express.static('images'))
 
 app.listen(3001, () => {
-	console.log("listening to port 3001...");
+    console.log("listening to port 3001...");
 })

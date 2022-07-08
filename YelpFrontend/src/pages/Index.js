15a1c308fd2790dc27c8c832689d7076f313ac90
Γιάννης Μικές <mikesgiannis@gmail.com>
2022-07-08 13:30:49 +0200

    formating

--- YelpFrontend/src/pages/Index.js
+++ YelpFrontend/src/pages/Index.js
@@ -79,79 +79,20 @@ const Index = () => {
 				<div className="flex w-1/6 items-center">
 					<button
 						onClick={() => setShowfilters(!showFilters)}
-						className=" cursor-pointer sm:flex hover:bg-gray-700 focus:ring focus:ring-offset-2 focus:ring-gray-800 py-4 px-6 bg-gray-800 flex text-base leading-4 font-normal text-white justify-center items-center "
-					>
-						<svg
-							className=" mr-2"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								d="M6 12C7.10457 12 8 11.1046 8 10C8 8.89543 7.10457 8 6 8C4.89543 8 4 8.89543 4 10C4 11.1046 4.89543 12 6 12Z"
-								stroke="white"
-								strokeWidth="1.5"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-							/>
-							<path
-								d="M6 4V8"
-								stroke="white"
-								strokeWidth="1.5"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-							/>
-							<path
-								d="M6 12V20"
-								stroke="white"
-								strokeWidth="1.5"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-							/>
-							<path
-								d="M12 18C13.1046 18 14 17.1046 14 16C14 14.8954 13.1046 14 12 14C10.8954 14 10 14.8954 10 16C10 17.1046 10.8954 18 12 18Z"
-								stroke="white"
-								strokeWidth="1.5"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-							/>
-							<path
-								d="M12 4V14"
-								stroke="white"
-								strokeWidth="1.5"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-							/>
-							<path
-								d="M12 18V20"
-								stroke="white"
-								strokeWidth="1.5"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-							/>
-							<path
-								d="M18 9C19.1046 9 20 8.10457 20 7C20 5.89543 19.1046 5 18 5C16.8954 5 16 5.89543 16 7C16 8.10457 16.8954 9 18 9Z"
-								stroke="white"
-								strokeWidth="1.5"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-							/>
-							<path
-								d="M18 4V5"
-								stroke="white"
-								strokeWidth="1.5"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-							/>
-							<path
-								d="M18 9V20"
-								stroke="white"
-								strokeWidth="1.5"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-							/>
+						className=" cursor-pointer sm:flex hover:bg-gray-700 focus:ring focus:ring-offset-2 focus:ring-gray-800 py-4 px-6 bg-gray-800 flex text-base leading-4 font-normal text-white justify-center items-center ">
+						<svg className=" mr-2" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" >
+							<path d="M6 12C7.10457 12 8 11.1046 8 10C8 8.89543 7.10457 8 6 8C4.89543 8 4 8.89543 4 10C4 11.1046 4.89543 12 6 12Z"
+								stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+							<path d="M6 4V8" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+							<path d="M6 12V20" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+							<path d="M12 18C13.1046 18 14 17.1046 14 16C14 14.8954 13.1046 14 12 14C10.8954 14 10 14.8954 10 16C10 17.1046 10.8954 18 12 18Z"
+								stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+							<path d="M12 4V14" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+							<path d="M12 18V20" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+							<path d="M18 9C19.1046 9 20 8.10457 20 7C20 5.89543 19.1046 5 18 5C16.8954 5 16 5.89543 16 7C16 8.10457 16.8954 9 18 9Z"
+								stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+							<path d="M18 4V5" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+							<path d="M18 9V20" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
 						</svg>
 						Filters
 					</button>
@@ -160,40 +101,30 @@ const Index = () => {
 					<Search onSearchAction={onSearchAction} />
 				</div>
 				<div className="flex w-1/6 justify-end">
-					<a
-						href="http://localhost:3000/mapview"
-						className="mx-2 my-2 bg-white transition duration-150 ease-in-out hover:border-indigo-600 hover:text-indigo-600 rounded border border-indigo-700 text-indigo-700 px-8 py-3 text-sm "
-					>
+					<a href="http://localhost:3000/mapview"
+						className="mx-2 my-2 bg-white transition duration-150 ease-in-out hover:border-indigo-600 hover:text-indigo-600 rounded border border-indigo-700 text-indigo-700 px-8 py-3 text-sm ">
 						Map view
 					</a>
 				</div>
 				<div className="flex w-1/6 justify-end">
-					<button
-						onClick={onClick}
-						className="mx-2 my-2 bg-white transition duration-150 ease-in-out hover:border-indigo-600 hover:text-indigo-600 rounded border border-indigo-700 text-indigo-700 px-8 py-3 text-sm "
-					>
+					<button onClick={onClick}
+						className="mx-2 my-2 bg-white transition duration-150 ease-in-out hover:border-indigo-600 hover:text-indigo-600 rounded border border-indigo-700 text-indigo-700 px-8 py-3 text-sm ">
 						Statistics
 					</button>
 				</div>
 			</div>
 
 			<div className="flex flex-row pt-24">
-				<div
-					className={`sticky h-full self-start top-24 ${showFilters ? "w-1/4" : "hidden"
-						}`}
-				>
+				<div className={`sticky h-full self-start top-24 ${showFilters ? "w-1/4" : "hidden"}`}>
 					<Filtering showFilters={showFilters} />
 				</div>
 
-				<div
-					className={`flex flex-col place-content-center overflow-hidden ${showFilters ? "w-3/4" : "w-full"
-						}`}
-				>
+				<div className={`flex flex-col place-content-center overflow-hidden ${showFilters ? "w-3/4" : "w-full"}`}>
 					{" "}
 					<div className="flex justify-center h-96">
 						<div className="flex justify-center cursor-pointer m-5 rounded-lg bg-white shadow-xl overflow-hidden w-full max-w-6xl">
 							<div className="flex flex-col lg:flex-row w-full items-start lg:items-center rounded">
-								<Map data={pageData}></Map>
+								<Map data={pageData} zoom={5}></Map>
 							</div>
 						</div>
 					</div>
@@ -209,8 +140,7 @@ const Index = () => {
 								onClick={() => goToPage(page - 1)}
 								className="mx-2 my-2 bg-white transition duration-150 ease-in-out hover:border-indigo-600
 			 hover:text-indigo-600 rounded border border-indigo-700 text-indigo-700 px-8 py-3 text-sm"
-								s
-							>
+								s>
 								Previous
 							</button>
 							{page}/{pageCount}
